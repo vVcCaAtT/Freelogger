f15b97fd194687be61942baa6faa6f7f0f552c05
vVcCaAtT <93036447+vVcCaAtT@users.noreply.github.com>
2021-11-08 18:29:57 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyAWzUf-TYQAMLKaPjrgIHHHvfRgTKhqOXE",
+  authDomain: "story-jo-bakabk-kartha-hai.firebaseapp.com",
+  databaseURL: "https://story-jo-bakabk-kartha-hai-default-rtdb.firebaseio.com",
+  projectId: "story-jo-bakabk-kartha-hai",
+  storageBucket: "story-jo-bakabk-kartha-hai.appspot.com",
+  messagingSenderId: "645870353283",
+  appId: "1:645870353283:web:d577f8caf92d23825bd4b6"
+};
